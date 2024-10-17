c8a00ccaa39590c9c5635125a149002cc0afd970
yangje0ngwoo <wood4704@gmail.com>
2024-10-18 03:48:42 +0900

    Add files via upload

---
+++ DQN-Car-Racing-master/src/CNN.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class CNNActionValue(nn.Module):
+    def __init__(self, state_dim, action_dim, activation=F.relu):
+        super(CNNActionValue, self).__init__()
+        self.conv1 = nn.Conv2d(state_dim, 16, kernel_size=8, stride=4)  # [N, 4, 84, 84] -> [N, 16, 20, 20]
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=4, stride=2)  # [N, 16, 20, 20] -> [N, 32, 9, 9]
+        self.in_features = 32 * 9 * 9
+        self.fc1 = nn.Linear(self.in_features, 256)
+        self.fc2 = nn.Linear(256, action_dim)
+        self.activation = activation
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = x.view((-1, self.in_features))
+        x = self.fc1(x)
+        x = self.fc2(x)
+        return x
